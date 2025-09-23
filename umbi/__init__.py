85fad90ffa9c333b91ab6b697bf1d4acd7551383
Roman Andriushchenko <iandri@fit.vut.cz>
2025-09-23 15:43:12 +0200

    Fix imports

--- umbi/__init__.py
+++ umbi/__init__.py
@@ -1,33 +1,24 @@
 import os
 import tomllib
 
-from .io.index import UmbIndex
 from .logger import set_log_level, setup_logging
+from . import binary
+from . import vectors  
+from . import io
 
-import umbi.binary as binary
-import umbi.vectors as vectors
-import umbi.io as io
-
-__all__ = [
-    "set_log_level",
-    "setup_logging",
-    "binary",
-    "vectors",
-    "io"
-]
-
-def get_pyproject_attribute(attribute, default):
+def get_pyproject_attribute(attribute, default, section="project"):
     """Read an attribute from pyproject.toml."""
     pyproject_path = os.path.join(os.path.dirname(__file__), "..", "pyproject.toml")
     try:
         with open(pyproject_path, "rb") as f:
-            project_data = tomllib.load(f)["project"]
-            return project_data.get(attribute)
+            data = tomllib.load(f)
+            section_data = data.get(section, {})
+            return section_data.get(attribute, default)
     except (FileNotFoundError, KeyError):
         return default
 
 
 __toolname__ = get_pyproject_attribute("name", "unknown")
 __version__ = get_pyproject_attribute("version", "unknown")
-__format_version__ = get_pyproject_attribute("format_version", 0)
-__format_revision__ = get_pyproject_attribute("format_revision", 1)
+__format_version__ = get_pyproject_attribute("format_version", 0, "tool.umbi")
+__format_revision__ = get_pyproject_attribute("format_revision", 1, "tool.umbi")
